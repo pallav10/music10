f13d92fb26695e282e066e05c7b1798ce511c2e0
Pallav <pallav8parikh@gmail.com>
2017-05-20 16:57:10 +0530

    adding host to ALLOWED_HOSTS

--- Music10/settings.py
+++ Music10/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'cf-7mh46ipkq_6b*j!rhcv@otsjj&tq5640sio(vc#cg2*87cc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0']
+ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'https://peaceful-reef-13157.herokuapp.com/']
 
 
 # Application definition
