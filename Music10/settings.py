e876cc6f03c73b3803dc979095be0e2930149ccf
Pallav <pallav8parikh@gmail.com>
2017-05-22 00:31:51 +0530

    trying fixes for loading static over heroku

--- Music10/settings.py
+++ Music10/settings.py
@@ -147,5 +147,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
