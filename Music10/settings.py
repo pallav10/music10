c2a87063f26aac345d5d7054c024b14c9d5cc870
Pallav <pallav8parikh@gmail.com>
2017-05-21 23:59:45 +0530

    changing db url

--- Music10/settings.py
+++ Music10/settings.py
@@ -85,20 +85,20 @@ WSGI_APPLICATION = 'Music10.wsgi.application'
 #     }
 # }
 
-# DATABASES = {
-#     'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'music10',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres123',
-        'HOST': '127.0.0.1',
-        'PORT': 5432,
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'music10',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres123',
+#         'HOST': '127.0.0.1',
+#         'PORT': 5432,
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
